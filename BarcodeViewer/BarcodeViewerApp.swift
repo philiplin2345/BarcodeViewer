3113259301e8e226804c59faf30b97824c4afb2e
Philip Lin <philiplin2345@gmail.com>
2026-04-27 08:44:52 +0800

    Initial Commit

---
+++ BarcodeViewer/BarcodeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeViewerApp.swift
+//  BarcodeViewer
+//
+//  Created by Philip Lin on 2026/4/27.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeViewerUITests/BarcodeViewerUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  BarcodeViewerUITestsLaunchTests.swift
+//  BarcodeViewerUITests
+//
+//  Created by Philip Lin on 2026/4/27.
+//
+
+import XCTest
+
+final class BarcodeViewerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
